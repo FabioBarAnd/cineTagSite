f7c14846407501f10820448297f25bafbc391509
Fabio Andrade <135459469+FabioBarAnd@users.noreply.github.com>
2024-06-08 15:49:54 -0300

    Add files via upload

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import styles from './Rodape.module.css';
+
+function Rodape() {
+    return(
+        <footer className={styles.footer}>
+            Desenvolvido por Fabio com ajuda de Alura.
+        </footer>
+    )
+}
+
+export default Rodape;
